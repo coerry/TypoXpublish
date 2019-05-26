085497e9c51488dc7dd658650a760ada2d81dbf1
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-05-26 20:31:33 +0200

    add firebase support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+var firebase = {
+  apiKey: "AIzaSyD5kytDwmZmNqQosvsDgsWs_46l4sGYY5E",
+  authDomain: "typox-3860f.firebaseapp.com",
+  databaseURL: "https://typox-3860f.firebaseio.com",
+  projectId: "typox-3860f",
+  storageBucket: "typox-3860f.appspot.com",
+  messagingSenderId: "432449450204",
+  appId: "1:432449450204:web:558396ed1e89bcb0"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AngularFireModule.initializeApp(firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
